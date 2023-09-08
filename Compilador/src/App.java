2ec87f06bb50b4a956d9cdc7bb8ae7ff35fb1eaf
Tomas Toniut <tomastoniut@advenio.com.ar>
2023-09-08 09:27:22 -0300

    feat: agrega lectura del archivo en el main

--- Compilador/src/App.java
+++ Compilador/src/App.java
@@ -1,5 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import lexicalanalyzer.LexicalAnalizer;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+                if (args.length != 1) {
+            System.out.println("Por favor, proporciona el nombre del archivo de texto como argumento.");
+            return;
+        }
+
+        String archivoTexto = args[0];
+
+        LexicalAnalizer lexicalAnalizer = new LexicalAnalizer(archivoTexto);
+
     }
 }
