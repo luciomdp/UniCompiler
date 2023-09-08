7726e9859f166ed3e8b28ff3712c157c3c9f77df
luciomdp <luchogpaulino@gmail.com>
2023-09-08 09:59:09 -0300

    Feat agrega JFileChooser

--- Compilador/src/App.java
+++ Compilador/src/App.java
@@ -1,13 +1,51 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
 import lexicalanalyzer.LexicalAnalizer;
 
 public class App {
+
+    private static JFileChooser fileChooser = new JFileChooser();
+    private static String path;
+
+    private static LexicalAnalizer lexicalAnalizer;
+
     public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
-            System.out.println("Por favor, proporciona el nombre del archivo de texto como argumento.");
-            return;
-        }
-        String archivoTexto = args[0];
-        LexicalAnalizer lexicalAnalizer = new LexicalAnalizer(archivoTexto);
+
+        do {
+            fileChooser.setDialogTitle("Elegí el archivo a compilar");
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            fileChooser.setAcceptAllFileFilterUsed(false);
+            //fileChooser.setCurrentDirectory(new File("C:\\Programacion\\Trabajo"));
+            try {
+                if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) 
+                 path = fileChooser.getSelectedFile().getAbsolutePath();
+                else
+                    Thread.currentThread().stop();
+            } catch (NullPointerException e) {
+                JOptionPane.showMessageDialog(null,(String)"No se ha seleccionado ningún archivo");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }while(path == null || !readFiles(path));
+
+        System.out.println(lexicalAnalizer.getToken());
 
     }
+    public static boolean readFiles(String path) {
+        try {
+            lexicalAnalizer = new LexicalAnalizer(path);
+            return true;
+        }catch(FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+        
+    }
+  
 }
