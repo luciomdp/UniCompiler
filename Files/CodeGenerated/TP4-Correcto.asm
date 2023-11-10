db69a4630bf5acdb1cd5356fe0acb9c4b643033b
luciopaulino <luciopaulino@advenio.com>
2023-11-10 18:55:32 -0300

    Feat quita cosas del assembler

--- Files/CodeGenerated/TP4-Correcto.asm
+++ Files/CodeGenerated/TP4-Correcto.asm
@@ -7,17 +7,17 @@ include \masm32\include\user32.inc
 includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\user32.lib
 .data
-     b.programa1 dw ?
-     a.programa1 dw ?
+     _b.programa1 dw ?
+     _a.programa1 dw ?
      @aux2 dw ?
      @aux1 dw ?
 .code
 start:
-     MOV eax, _a.programa1
-     MOV _1, eax 
+     MOV eax, 1
+     MOV _a.programa1, eax 
 
-     MOV eax, _b.programa1
-     MOV _2, eax 
+     MOV eax, 2
+     MOV _b.programa1, eax 
 
      invoke ExitProcess, 0
 end start
